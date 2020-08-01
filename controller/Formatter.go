11e122f95b1d4d4e6656647401c43b35822a4338
宮良一生 <e195755@ie.u-ryukyu.ac.jp>
2020-08-01 23:38:22 +0900

    add test

--- controller/Formatter.go
+++ controller/Formatter.go
@@ -1,16 +1,61 @@
 package controller
 
-import "time"
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+const (
+	TIME    = 0
+	RXLEVEL = 1
+)
 
 type Formatter struct {
+	Average             int
+	AveragePreviousFile int
 }
 
-func (f *Formatter) FormatData(data []string) []string {
-	panic("not implement")
+func NewFormatter() *Formatter {
+	return &Formatter{}
 }
 
-func (f *Formatter) GetAve() *time.Time {
-	panic("not implement")
+func (f *Formatter) FormatData(data [][]string) [][]string {
+	panic("not imprement")
+	/*
+		basetime := f.dataToTime("00:00:00")
+
+		for _, daily_data := range data{
+
+		}
+
+	*/
+}
+
+func (f *Formatter) GetAve(data [][]string, baseTime *time.Time) ([]string, error) {
+	ave := 0
+	i := 0
+	subTime := baseTime.Add(time.Second*10)
+	for i = 0; i < len(data); i++ {
+		//a := f.dataToTime(data[i][TIME]).Second()
+		//sub := f.dataToTime(subTime.Sub(f.dataToTime(data[i][TIME]))).Second()
+		tmp := subTime.Sub(f.dataToTime(data[i][TIME]))
+		t := (baseTime.Hour()*60*60) + (baseTime.Minute()*60) + baseTime.Second()
+		if t<= int(tmp.Seconds()){
+			n, err := strconv.Atoi(data[i][RXLEVEL])
+			if err != nil {
+				panic(err.Error())
+			}
+			ave += n
+		} else {
+			if ave == 0{
+				return nil, fmt.Errorf("Average cannot be defined")
+			}
+			break
+		}
+	}
+	records := []string{f.timeToData(*baseTime), strconv.Itoa(ave / i)}
+	return records, nil
 }
 
 func (f *Formatter) GetAveAfter() *time.Time {
@@ -27,4 +72,13 @@ func (f *Formatter) SetAve(time *time.Time) {
 
 func (f *Formatter) SetAvePreviousFile(time *time.Time) {
 	panic("not implement")
+}
+
+func (f *Formatter) dataToTime(date string) time.Time {
+	t, _ := time.Parse("15:04:05", date)
+	return t
+}
+
+func (f *Formatter) timeToData(t time.Time) string {
+	return t.Format("15:04:05")
 }
--- controller/Formatter_test.go
+++ controller/Formatter_test.go
@@ -1,1 +1,99 @@
 package controller
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestFormatter_GetAve(t *testing.T) {
+	type fields struct {
+		Average             int
+		AveragePreviousFile int
+	}
+	type args struct {
+		data     [][]string
+		baseTime *time.Time
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    []string
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "not exception test",
+			fields: fields{
+				Average:             0,
+				AveragePreviousFile: 0,
+			},
+			args: args{
+				data: [][]string{
+					{"00:00:02", "-5"},
+					{"00:00:05", "-7"},
+				},
+				baseTime: testFuncDataTime("00:00:00"),
+			},
+			want: []string{"00:00:00", "-6"},
+		},
+		{
+			name:   "overflow data",
+			fields: fields{},
+			args: args{
+				data: [][]string{
+					{"00:00:02", "-5"},
+					{"00:00:05", "-7"},
+					{"00:00:07", "-8"},
+					{"00:00:08", "-12"},
+					{"00:00:12", "-7"},
+				},
+				baseTime: testFuncDataTime("00:00:00"),
+			},
+			want:    []string{"00:00:00", "-8"},
+			wantErr: false,
+		},
+		{
+			name:    "NoData",
+			fields:  fields{
+				Average: -5,
+				AveragePreviousFile: 0,
+			},
+			args:    args{
+				data:    [][]string{
+			{"00:01:02", "-5"},
+			{"00:01:05", "-7"},
+			{"00:01:07", "-8"},
+			{"00:01:08", "-12"},
+			{"00:01:12", "-7"},
+			},
+				baseTime: testFuncDataTime("00:00:00"),
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &Formatter{
+				Average:             tt.fields.Average,
+				AveragePreviousFile: tt.fields.AveragePreviousFile,
+			}
+			got, err := f.GetAve(tt.args.data, tt.args.baseTime)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetAve() got = %v, want %v", got, tt.want)
+			}
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetAve() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
+
+func testFuncDataTime(date string) *time.Time {
+	t, _ := time.Parse("15:04:05", date)
+	return &t
+}
