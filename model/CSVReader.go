6a585a300a31a137a77b9d568065215c6d7472bb
宮良一生 <e195755@ie.u-ryukyu.ac.jp>
2020-08-01 11:40:07 +0900

    add CSVReader and test

--- model/CSVReader.go
+++ model/CSVReader.go
@@ -1,1 +1,63 @@
 package model
+
+import (
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+type CSVReader struct {
+}
+
+func (c *CSVReader) GetData(filepath string) ([][]string, error) {
+	f, err := c.ReadFile(filepath)
+	if err != nil {
+		return nil, err
+	}
+	r := csv.NewReader(f)
+
+	row := 2
+	if err := c.removeHead(row, r); err != nil {
+		fmt.Println(err.Error())
+	}
+	return c.parseCSV(r), nil
+}
+
+func (c *CSVReader) ReadFile(filepath string) (*os.File, error) {
+	//指定したファイルが存在しなければ
+	_, err := os.Stat(filepath)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+
+	f, err := os.Open(filepath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return f, nil
+}
+
+func (c *CSVReader) parseCSV(csv *csv.Reader) [][]string {
+	var records [][]string
+	for {
+		record, err := csv.Read()
+		if err == io.EOF {
+			break
+		}
+		records = append(records, record)
+	}
+	return records
+}
+
+func (c *CSVReader) removeHead(n int, csv *csv.Reader) error {
+	for i := 0; i < 2; i++ {
+		_, err := csv.Read()
+		if err == io.EOF {
+			return errors.New("file exists but not wrote")
+		}
+	}
+	return nil
+}
--- model/CSVReader_test.go
+++ model/CSVReader_test.go
@@ -1,1 +1,85 @@
 package model
+
+import (
+	"encoding/csv"
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestCSVReader_removeHead(t *testing.T) {
+	type args struct {
+		n    int
+		file *csv.Reader
+	}
+	f, err := os.Open("removeHeadTestData.csv")
+	if err != nil {
+		t.Errorf("removeHeadTestData.csv not found")
+	}
+	defer f.Close()
+	r := csv.NewReader(f)
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "removeHeadTestData.csv",
+			args: args{
+				n:    2,
+				file: r,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &CSVReader{}
+			if err := c.removeHead(tt.args.n, tt.args.file); (err != nil) != tt.wantErr {
+				t.Errorf("removeHead() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestCSVReader_parseCSV(t *testing.T) {
+	type args struct {
+		csv *csv.Reader
+	}
+
+	f, err := os.Open("removeHeadTestData.csv")
+	if err != nil {
+		t.Errorf("removeHeadTestData.csv not found")
+	}
+	defer f.Close()
+	r := csv.NewReader(f)
+
+	tests := []struct {
+		name string
+		args args
+		want [][]string
+	}{
+		// TODO: Add test cases.
+		{
+			name: "ReadCSVTEST",
+			args: args{
+				csv: r,
+			},
+			want: [][]string{
+				{"removeHead"},
+				{"removeHead2"},
+				{"Body", "Hoge"},
+				{"Body2", "Fuga"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &CSVReader{}
+			if got := c.parseCSV(tt.args.csv); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseCSV() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
